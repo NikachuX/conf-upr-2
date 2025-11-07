71a2a6a93c1e8b39541e4606f86086db2ea909d7
Nikita Boriskin <nikitaboriskin051@yandex.ru>
2025-11-08 00:42:43 +0300

    Stage 2.1

--- main.py
+++ main.py
@@ -1,37 +1,6 @@
 import sys
-import os
 from validate import validate_args, parse_args
-import requests
-
-def parse_cargo_toml(path):
-    deps = []
-    in_deps_section = False
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
-            for line in f:
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                if line.startswith('[dependencies]'):
-                    in_deps_section = True
-                    continue
-                if line.startswith('[') and not line.startswith('[dependencies]'):
-                    in_deps_section = False
-                if in_deps_section and '=' in line:
-                    key, value = line.split('=', 1)
-                    deps.append(key)
-    except Exception as e:
-        raise RuntimeError(f"Ошибка при чтении Cargo.toml: {e}")
-    return deps
-
-def get_toml(url):
-    response = requests.get(url)
-    content = response.text
-    with open('dep.toml', 'w', encoding='utf-8') as f:
-        f.write(content)
-    deps = parse_cargo_toml("dep.toml")
-    os.remove("dep.toml")
-    return deps
+from parsing import get_toml, get_deps_by_name
 
 
 def main():
@@ -57,7 +26,7 @@ def main():
         "depth": args.depth,
     }
 
-    deps = get_toml(kv['repo'])
+    deps = get_deps_by_name(kv['package'], kv['version'])
     print(deps)
 
 if __name__ == '__main__':
